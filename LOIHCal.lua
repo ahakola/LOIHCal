a8965375a1c8ec37ba670e398aa3e802eb46e7f9
Unknown <40722602+ahakola@users.noreply.github.com>
2023-02-22 02:02:20 +0200

    Update year

--- LOIHCal.lua
+++ LOIHCal.lua
@@ -1,7 +1,7 @@
 --[[----------------------------------------------------------------------------
 	LOIHCal
 
-	2014-2022
+	2014-2023
 	Sanex @ EU-Arathor / ahak @ Curseforge
 
 	http://wow.curseforge.com/addons/loihcal/
