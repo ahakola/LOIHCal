8e851dfa63e4adeddba2b7c13e449850b401c3ee
Unknown <40722602+ahakola@users.noreply.github.com>
2024-01-17 16:50:52 +0200

    Current year is 2024

--- LOIHCal.lua
+++ LOIHCal.lua
@@ -1,7 +1,7 @@
 --[[----------------------------------------------------------------------------
 	LOIHCal
 
-	2014-2023
+	2014-2024
 	Sanex @ EU-Arathor / ahak @ Curseforge
 
 	http://wow.curseforge.com/addons/loihcal/
